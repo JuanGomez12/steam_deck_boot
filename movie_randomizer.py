85eddc3e9aa3256bc3a1ce6004f419f837fc2621
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2022-10-15 00:40:01 -0400

    Work on the movie randomizer

---
+++ movie_randomizer.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import random
+import logging
+
+logging.basicConfig()
+
+def randomize_boot_movie():
+    movies_path = Path('uioverrides') / 'movies'
+    startup_movie_old = movies_path / 'deck_startup.webm'
+    if movies_path.is_dir():
+        boot_movies = sorted(list(movies_path.glob('*deck_startup.webm')) + list(movies_path.glob('*deck_startup.WEBM')))
+        if boot_movies:
+            random_movie = random.randint(0, len(boot_movies) - 1)
+            new_boot_movie = boot_movies[random_movie]
+            if startup_movie_old.exists():
+                # Rename current movie
+                counter=1
+                while startup_movie_old.exists():
+                    startup_movie = movies_path / f'old_{counter}_{startup_movie_old.name}'
+                    counter+=1
+                if not startup_movie_old.exists():
+                    logging.info(f'Renaming {startup_movie_old} to {startup_movie}')
+                    startup_movie.rename(startup_movie_old)
+            else:
+                # Rename new movie to deck_startup.webm
+                pass
+        else:
+            logging.warning(f'Could not find movies in {movies_path}')
+    else:
+        logging.warning(f'Could not find the uioverrides/movies directory in {movies_path.resolve().parents[2]}')
+
+if __name__ == "__main__":
+    randomize_boot_movie()
